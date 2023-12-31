944d1f0acc60decd03823f944f1f5248e36beb9e
opurcar <opurcar@autoteorica.com>
2023-12-31 10:55:43 +0100

    dinamically add books to table

--- library_project/script.js
+++ library_project/script.js
@@ -1,7 +1,3 @@
-// create myLibrary constant to store added books
-
-const myLibrary = [];
-
 // this is my constructor formula
 function Book(book, author, pages) {
     this.book = book;  
@@ -17,7 +13,6 @@ function addBookToLibrary(book, author, pages) {
 }
 
 
-
 function submitForm(event) {
     event.preventDefault(); // Prevent the form from submitting in the traditional way
 
@@ -30,15 +25,52 @@ function submitForm(event) {
     const numPages = numPagesInput.value;
 
     addBookToLibrary(bookTitle, author, numPages);
+    displayBooks();
 
     // Clear the form fields after submitting
     bookTitleInput.value = '';
     authorInput.value = '';
     numPagesInput.value = '';
-
-    // Log the updated library to the console
-    console.log(myLibrary);
     
 }
 
-    // function that loops through array and display each book
+// create myLibrary constant to store added books
+
+const myLibrary = [];
+
+
+// function to display books in the table
+function displayBooks() {
+    const tableBody = document.querySelector('table tbody');
+    
+    // Clear existing rows in the table
+    tableBody.innerHTML = '';
+
+    // Loop through the myLibrary array and add each book to the table
+    myLibrary.forEach((book, index) => {
+        const row = tableBody.insertRow(); // Create a new row
+
+        // Insert cells for book title, author, number of pages, and a remove button
+        const titleCell = row.insertCell(0);
+        const authorCell = row.insertCell(1);
+        const pagesCell = row.insertCell(2);
+        const removeCell = row.insertCell(3);
+
+        titleCell.textContent = book.book;
+        authorCell.textContent = book.author;
+        pagesCell.textContent = book.pages;
+
+        // Create a remove button and attach a click event to remove the book from the library
+        const removeButton = document.createElement('button');
+        removeButton.textContent = 'Remove';
+        removeButton.addEventListener('click', () => removeBook(index));
+        removeCell.appendChild(removeButton);
+    });
+}
+
+// function to remove a book from the library
+function removeBook(index) {
+    myLibrary.splice(index, 1); // Remove the book from the array
+    displayBooks(); // Update the table to reflect the changes
+}
+
